b3d75ac5f7a836c40060ba3d14a6b2b8045eaffe
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-12 19:24:13 -0300

    feat: esqueleto da Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import FrameButton from "../../components/ActivityButton";
 import { Container, Grid, Section } from "../../styles/Containers";
-import { Text, Title, TopicTitle } from "../../styles/Text";
+import { Text, Title, SubTitle } from "../../styles/Text";
 import imageEsportes from "../../assets/img/esportes.jpg";
 import imageCultura from "../../assets/img/cultura.jpg";
 import imageLazer from "../../assets/img/lazer.jpg";
@@ -11,21 +11,7 @@ import { StyledHome } from "./styles";
 const Home = () => {
   return (
     <StyledHome>
-      <img src={imageFaixa} width="100%" />
-      <Container>
-        <Section id="quem-somos">
-          <TopicTitle color="orange">
-            O que é? O projeto Cidade Ativa
-          </TopicTitle>
-          <Text>
-            O Projeto Cidade Ativa é um projeto sem fins lucrativos, com a
-            missão de democratizar o acesso de crianças, jovens e adultos ao
-            Esporte, Cultura e Lazer; Trazendo uma oportunidade para melhor
-            qualidade de vida, despertando sonhos e impulsionando-os para que se
-            tornam realidade
-          </Text>
-        </Section>
-      </Container>
+      <Container></Container>
     </StyledHome>
   );
 };

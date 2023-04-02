a7693638433edfeb2b31676883d59bde78136811
Micaias Silva <micaiasilvaop@gmail.com>
2023-04-01 22:26:11 -0300

    feat: adicionadas transições e estados para os elementos de formulário

--- src/styles/Form.tsx
+++ src/styles/Form.tsx
@@ -12,13 +12,40 @@ export const Input = styled("input", {
   display: "block",
   padding: "10px ",
   width: "100%",
-  border: "none",
+  border: "transparent 2px solid",
   height: 50,
   borderRadius: 5,
-  marginBottom: 20,
+  transition: "0.2s",
+  "&:disabled": {
+    cursor: "not-allowed",
+    background: "$gray2",
+  },
 
   "&:focus": {
     boxShadow: "0 0 0 0",
     outline: "none",
+    border: "$gray 2px solid",
+  },
+});
+
+export const TextArea = styled("textarea", {
+  fontFamily: "Inter",
+  resize: "none",
+  display: "block",
+  padding: "10px ",
+  width: "100%",
+  height: 150,
+  border: "transparent 2px solid",
+  borderRadius: 5,
+  transition: "0.2s",
+  "&:disabled": {
+    cursor: "not-allowed",
+    background: "$gray2",
+  },
+
+  "&:focus": {
+    boxShadow: "0 0 0 0",
+    outline: "none",
+    border: "$gray 2px solid",
   },
 });

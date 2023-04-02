d19ad170851c4dd81187b3241aa33e87c3250672
Micaias Silva <micaiasilvaop@gmail.com>
2023-04-01 22:21:08 -0300

    feat: implementação do toast na página

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,24 @@ import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import { Wrapper } from "./styles/Containers";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <Wrapper className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <Navigation />
       <Header />
       <Home />

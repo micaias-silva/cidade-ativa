bcc2ff389adc3f3ef8a83ff5883cbafd0a39b5bb
Micaias Silva <micaiasilvaop@gmail.com>
2023-04-01 22:23:47 -0300

    feat: adicionado objeto de configurações para o emailjs

--- src/config/contactData.ts
+++ src/config/contactData.ts
@@ -1,5 +1,13 @@
 export const contactData = {
   instagram: "https://www.instagram.com/cidadeativa.mcz/",
   phoneNumbers: ["82981608392", "82998087671"],
-  emails: ["institutocidadeativa.mcz@gmail.com"],
+  emails: ["augustosimas.al@gmail.com"],
 };
+
+export const emailJsConfig = {
+  serviceId: (import.meta.env.VITE_SERVICE_ID as string) || "",
+  templateId: (import.meta.env.VITE_TEMPLATE_ID as string) || "",
+  publicKey: (import.meta.env.VITE_PUBLIC_KEY as string) || "",
+};
+
+console.log(emailJsConfig);

ffc1c730b6ee42f74996dbc906c67f646fa2d764
Micaias Silva <micaiasilvaop@gmail.com>
2024-03-04 07:53:12 -0300

    feat: remade navbar styling structure and added animation for navbar view toggle

--- src/components/Navigation/styles.tsx
+++ src/components/Navigation/styles.tsx
@@ -1,16 +1,48 @@
+import { keyframes } from "@stitches/react";
+import { fadeIn } from "@styles/Animations";
 import { Flex } from "@styles/Containers";
 import { styled } from "@styles/stitches.config";
 
+const ToggleView = keyframes({
+  from: {
+    top: 0,
+  },
+  to: {
+    top: -80,
+  },
+});
+
 export const NavigationContainer = styled("div", {
+  position: "fixed",
   height: 80,
-  width: "100%",
-  padding: "0 2rem",
   top: 0,
   left: 0,
-  position: "absolute",
-  zIndex: 5,
-  [`& ${Flex}`]: {
-    height: "100%",
+  zIndex: 2,
+  width: "100%",
+  ".nav-wrapper": {
+    height: 80,
+    width: "100%",
+    top: 0,
+    left: 0,
+    position: "fixed",
+    zIndex: 2,
+    transition: ".2s",
+    padding: "0 2rem",
+    justifyContent: "space-between",
+  },
+  ".show-background": {
+    background: "$orange",
+  },
+  ".toggle-showing": {
+    animation: ToggleView,
+    animationDuration: "0.5s",
+    animationFillMode: "forwards",
+    animationDirection: "reverse",
+  },
+  ".toggle-hiding": {
+    animation: ToggleView,
+    animationDuration: "0.5s",
+    animationFillMode: "forwards",
   },
 });
 

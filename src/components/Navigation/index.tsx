172a6628a455c695f70c94e463daacc05fa55d83
Micaias Silva <micaiasilvaop@gmail.com>
2024-03-04 07:52:18 -0300

    feat: scrolling handler for scrolling-responsive navbar

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -2,18 +2,43 @@ import { Flex } from "@styles/Containers";
 import { GenericLink } from "@styles/Text";
 import Logo from "@components/Logo";
 import { NavigationContainer, StyledNav } from "./styles";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [isShowing, setShowing] = useState<boolean>(true);
+  const [viewBackground, setViewBackground] = useState<boolean>(false);
+
+  let lastScrollTop = window.scrollY;
+
+  window.addEventListener("scroll", (e) => {
+    const scrollTopPosition = window.scrollY;
+
+    if (scrollTopPosition < 200 || scrollTopPosition < lastScrollTop) {
+      setShowing(true);
+    } else if (scrollTopPosition > lastScrollTop) {
+      setShowing(false);
+    }
+
+    if (scrollTopPosition > 250) {
+      setViewBackground(true);
+    } else {
+      setViewBackground(false);
+    }
+
+    lastScrollTop = scrollTopPosition < 0 ? 0 : scrollTopPosition;
+  });
+
   return (
     <NavigationContainer>
-      <Flex>
+      <Flex className={`nav-wrapper `}>
         <Logo />
         <StyledNav>
           <Flex>
-            <GenericLink href="#projects">Projetos</GenericLink>
-            <GenericLink href="#about-us">Quem Somos</GenericLink>
-            <GenericLink href="#location">Onde Estamos?</GenericLink>
-            <GenericLink href="#contrib-form">Deseja Contribuir?</GenericLink>
+            <GenericLink href="#apoiadores">APOIADORES</GenericLink>
+            <GenericLink href="#nossos-projetos">PROJETOS</GenericLink>
+            <GenericLink href="#sobre-nos">SOBRE NÓS</GenericLink>
+            <GenericLink href="#localização">ONDE ESTAMOS</GenericLink>
+            <GenericLink href="#fale-conosco">FALE CONOSCO</GenericLink>
           </Flex>
         </StyledNav>
       </Flex>

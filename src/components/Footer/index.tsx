baf8a17c886f6c9ff413e33ab374fde8043581f2
Micaias Silva <micaiasilvaop@gmail.com>
2024-03-04 06:19:39 -0300

    feat: updated footer structure and added copyright footer message and horizontal line above it

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,52 +1,65 @@
-import { Container, Grid, ListItem, UnorderedList } from "@styles/Containers";
+import {
+  Container,
+  Flex,
+  Grid,
+  ListItem,
+  UnorderedList,
+} from "@styles/Containers";
 import imageLogo from "@img/logo.svg";
 import { GrInstagram } from "react-icons/gr";
 import { BsTelephone } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
 
-import { InGridContainer, SocialMediaLink, StyledFooter } from "./styles";
+import { InGridContainer, StyledFooter } from "./styles";
 import { ImageContainer } from "@styles/Image";
 import { contactData } from "@config/contactData";
-import { GenericLink, Topic, Text } from "@styles/Text";
+import { GenericLink, Topic, Text, Span, Subtitle } from "@styles/Text";
 import { formatPhone } from "@tools/format.tools";
 
 const Footer = () => {
   return (
     <StyledFooter>
       <Container>
-        <Grid>
-          <InGridContainer>
-            <ImageContainer>
-              <img src={imageLogo} alt="" />
-            </ImageContainer>
-          </InGridContainer>
-          <InGridContainer>
-            <Topic color="white">Entre em Contato</Topic>
-            <UnorderedList>
-              <ListItem>
-                <GrInstagram />
-                <SocialMediaLink href={contactData.instagram} target="_blank">
-                  Instagram
-                </SocialMediaLink>
-              </ListItem>
-              {contactData.phoneNumbers.map((phone, index) => (
-                <ListItem key={index}>
-                  <BsTelephone />
-                  <GenericLink href={`tel:${formatPhone(phone)}`}>
-                    {formatPhone(phone)}
+        <Flex className="splitter">
+          <ImageContainer>
+            <img src={imageLogo} alt="" />
+          </ImageContainer>
+          <Grid>
+            <InGridContainer>
+              <Subtitle color="white">Redes Sociais</Subtitle>
+              <UnorderedList>
+                <ListItem>
+                  <GrInstagram />
+                  <GenericLink href={contactData.instagram} target="_blank">
+                    Instagram
                   </GenericLink>
                 </ListItem>
-              ))}
-              {contactData.emails.map((email, index) => (
-                <ListItem key={"email" + index}>
-                  <AiOutlineMail />
-                  {email}
-                </ListItem>
-              ))}
-            </UnorderedList>
-          </InGridContainer>
-        </Grid>
+              </UnorderedList>
+            </InGridContainer>
+            <InGridContainer>
+              <Subtitle color="white">Entre em Contato</Subtitle>
+              <UnorderedList>
+                {contactData.phoneNumbers.map((phone, index) => (
+                  <ListItem key={index}>
+                    <BsTelephone />
+                    <GenericLink href={`tel:${formatPhone(phone)}`}>
+                      {formatPhone(phone)}
+                    </GenericLink>
+                  </ListItem>
+                ))}
+                {contactData.emails.map((email, index) => (
+                  <ListItem key={"email" + index}>
+                    <AiOutlineMail />
+                    <GenericLink href={`mailto:${email}`}>{email}</GenericLink>
+                  </ListItem>
+                ))}
+              </UnorderedList>
+            </InGridContainer>
+          </Grid>
+        </Flex>
       </Container>
+      <hr />
+      <Span color="gray3">©2024, TODOS OS DIREITOS RESERVADOS</Span>
     </StyledFooter>
   );
 };

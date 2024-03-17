5580971143c03bddaa0f4c524416b051df90daad
Micaias Silva <micaiasilvaop@gmail.com>
2024-03-17 07:47:48 -0300

    feat: mocked projects list for showcase while there's no backend

---
+++ src/config/mockedData.ts
@@ -0,0 +1,25 @@
+import { IProjectCard } from "@interfaces/index";
+import imageEsportes from "@img/esportes.jpg";
+
+export const ProjectsData: IProjectCard[] = [
+  {
+    title: "Empreendedorismo",
+    category: "Educação",
+    imgUrl: imageEsportes,
+  },
+  {
+    title: "Taekwondo",
+    category: "Esportes",
+    imgUrl: imageEsportes,
+  },
+  {
+    title: "Taekwondo",
+    category: "Esportes",
+    imgUrl: imageEsportes,
+  },
+  {
+    title: "Taekwondo",
+    category: "Esportes",
+    imgUrl: imageEsportes,
+  },
+];

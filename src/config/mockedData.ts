7e23298c383ff1b487495d5055af36fa08aeefc2
Micaias Silva <micaiasilvaop@gmail.com>
2024-03-19 10:22:51 -0300

    feat: renamed ProjectsData veriable to mockedProjectList

--- src/config/mockedData.ts
+++ src/config/mockedData.ts
@@ -1,24 +1,44 @@
 import { IProjectCard } from "@interfaces/index";
 import imageEsportes from "@img/esportes.jpg";
 
-export const ProjectsData: IProjectCard[] = [
+export const mockedProjectList: IProjectCard[] = [
   {
     title: "Empreendedorismo",
     category: "Educação",
     imgUrl: imageEsportes,
   },
   {
-    title: "Taekwondo",
+    title: "Taekwondo 1",
     category: "Esportes",
     imgUrl: imageEsportes,
   },
   {
-    title: "Taekwondo",
+    title: "Taekwondo 2",
     category: "Esportes",
     imgUrl: imageEsportes,
   },
   {
-    title: "Taekwondo",
+    title: "Taekwondo 3",
+    category: "Esportes",
+    imgUrl: imageEsportes,
+  },
+  {
+    title: "Empreendedorismo 2",
+    category: "Educação",
+    imgUrl: imageEsportes,
+  },
+  {
+    title: "Taekwondo 5",
+    category: "Esportes",
+    imgUrl: imageEsportes,
+  },
+  {
+    title: "Taekwondo 6",
+    category: "Esportes",
+    imgUrl: imageEsportes,
+  },
+  {
+    title: "Taekwondo 7",
     category: "Esportes",
     imgUrl: imageEsportes,
   },
